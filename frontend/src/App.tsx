58cfd052f10d89be58cbee9494657445ca60d7cc
yr7256 <yr7256@gmail.com>
2023-03-09 12:57:50 +0900

    feat: router 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Kakao from './Components/Kakao/Kakao';
-import Topbar from './Components/Topbar/Topbar';
+// import Kakao from './Components/Kakao/Kakao';
+// import Topbar from './Components/Topbar/Topbar';
+import Map from './pages/Map/Map';
 
 function App() {
 	return (
-		<div className="App">
-			<Topbar />
-			<Kakao />
-		</div>
+		// <div className="App">
+		// 	<Topbar />
+		// 	<Kakao />
+		// </div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/map" element={<Map />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,9 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
-			<Router>
-				<App />
-			</Router>
+		<App />
 	</React.StrictMode>
 );
 
