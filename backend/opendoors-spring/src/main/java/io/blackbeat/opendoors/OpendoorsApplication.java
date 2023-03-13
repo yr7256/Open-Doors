2c0f39ceef5642858a1e1c1f0846ae3161cf4dc5
leejinseop <jjjp9966@gmail.com>
2023-03-13 11:21:07 +0900

    dir: add initial project

---
+++ backend/opendoors-spring/src/main/java/io/blackbeat/opendoors/OpendoorsApplication.java
@@ -0,0 +1,13 @@
+package io.blackbeat.opendoors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpendoorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpendoorsApplication.class, args);
+	}
+
+}
