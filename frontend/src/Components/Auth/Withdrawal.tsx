60c4264c18de61bfc0887fb82ad2a501170cbe5f
yeahzizi <newyeahzizi@gmail.com>
2023-03-27 16:14:49 +0900

    feat: 회원탈퇴 구현

--- frontend/src/Components/Auth/Withdrawal.tsx
+++ frontend/src/Components/Auth/Withdrawal.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { ModalBackGround, ModalWrap, ModalContainer, P } from '../../styles/Auth/Withdrawalstyle';
+import { MiniButton } from '../../styles/Button/ButtonStyle';
+import { logout } from '../../store/Cookie';
+import { logoutAccount } from '../../store/AuthSlice';
 
-function Withdrawal() {
-	return <div />;
+type Props = {
+	title?: string;
+	alert?: string;
+	closeModal?: () => void;
+};
+
+type UserState = {
+	user: {
+		password: string;
+	};
+};
+
+function Withdrawal({ title, alert, closeModal }: Props) {
+	const password = useSelector((state: UserState) => state.user.password);
+	const dispatch = useDispatch();
+
+	const DeleteUser = () => {
+		const accessToken = localStorage.getItem('accessToken');
+		axios
+			.delete(`/api/user/${password}`, {
+				headers: { Authorization: `Bearer ${accessToken}` },
+			})
+			.then(() => {
+				console.log('회원탈퇴라니');
+				dispatch(logoutAccount());
+				logout();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<>
+			<ModalWrap>
+				<ModalBackGround onClick={closeModal} />
+				<ModalContainer>
+					<h2>{title}</h2>
+					<P>{alert}</P>
+					<div>
+						<MiniButton onClick={DeleteUser}>예</MiniButton>
+						<MiniButton onClick={closeModal}>아니오</MiniButton>
+					</div>
+				</ModalContainer>
+			</ModalWrap>
+		</>
+	);
 }
 
 export default Withdrawal;
--- frontend/src/pages/Profile/MyInfoManage.tsx
+++ frontend/src/pages/Profile/MyInfoManage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Head, Line, Title } from '../../styles/Menu/NavStyle';
-import { ThickLine, Ptag, Div } from '../../styles/Profile/MyPagestyle';
+import { ThickLine, Ptag, Div, Withdraw, Gray } from '../../styles/Profile/MyPagestyle';
+import Withdrawal from '../../Components/Auth/Withdrawal';
 import GoBackPage from '../../Components/Menu/goBackPage';
 import MyImg from '../../Components/Profile/MyImg';
 import Footer from '../../Components/Menu/Footer';
@@ -15,6 +16,14 @@ function MyInfoManage() {
 
 	const moveChangeBarrierFree = () => {
 		navigate('/Mypage/ChangeBarrierFree');
+	};
+
+	const [isOpenModal, setIsOpenModal] = useState(false);
+	const clickModal = () => {
+		setIsOpenModal(true);
+	};
+	const closeModal = () => {
+		setIsOpenModal(false);
 	};
 	return (
 		<>
@@ -39,6 +48,16 @@ function MyInfoManage() {
 				<Ptag>배리어프리 항목 변경</Ptag>
 			</Div>
 			<Line />
+			<Withdraw>
+				<Gray onClick={clickModal}>회원탈퇴</Gray>
+				{isOpenModal && (
+					<Withdrawal
+						closeModal={closeModal}
+						title="정말 탈퇴하시겠습니까?"
+						alert="탈퇴 시 모든 정보가 삭제되며, 복구되지 않습니다."
+					/>
+				)}
+			</Withdraw>
 			<Footer />
 		</>
 	);
