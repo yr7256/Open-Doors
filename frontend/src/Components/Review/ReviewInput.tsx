648085bd843158cc0d069f1c7d51d378f5ba34ca
yeahzizi <newyeahzizi@gmail.com>
2023-03-29 09:57:36 +0900

    chore: 리뷰 등록 코드 정리 및 css 추가

--- frontend/src/Components/Review/ReviewInput.tsx
+++ frontend/src/Components/Review/ReviewInput.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-import { Form, P } from '../../styles/Review/ReviewInputstyle';
+import { Form, P, Img } from '../../styles/Review/ReviewInputstyle';
 import { Button, PhotoButton } from '../../styles/Button/ButtonStyle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faSolidStar } from '@fortawesome/free-solid-svg-icons';
@@ -17,7 +17,7 @@ type UserState = {
 function ReviewInput() {
 	const [review, setReview] = useState('');
 	const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-	const [starScore, setStarScore] = useState(0);
+	const [starScore, setStarScore] = useState(3);
 	const score = [1, 2, 3, 4, 5];
 	const username = useSelector((state: UserState) => state.user.username);
 
@@ -32,10 +32,6 @@ function ReviewInput() {
 	};
 	const handleFileDelete = (file: File) => {
 		setSelectedFiles((prevSelectedFiles) => prevSelectedFiles.filter((prevFile) => prevFile !== file));
-	};
-	const handleFileUpload = () => {
-		// do something with the selected files
-		console.log(selectedFiles);
 	};
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -82,6 +78,8 @@ function ReviewInput() {
 									icon={starScore >= el ? faSolidStar : faRegularStar}
 									key={el}
 									onClick={() => setStarScore(el)}
+									size="2x"
+									color="#6393CB"
 								/>
 							))}
 						</StarContainer>
@@ -96,16 +94,18 @@ function ReviewInput() {
 				</div>
 				<div className="grid grid-cols-8 gap-1">
 					<div className="col-start-2 col-span-6">
-						<P>사진 등록</P>
+						<label htmlFor="add-image">
+							<P>사진 등록</P>
+						</label>
 						<PhotoButton>
-							<input type="file" multiple onChange={handleFileSelect} />
 							사진 추가
+							<input type="file" multiple onChange={handleFileSelect} id="add-image" />
 						</PhotoButton>
 						{selectedFiles.length > 0 && (
 							<ul>
 								{selectedFiles.map((file, index) => (
 									<li key={index}>
-										<img src={URL.createObjectURL(file)} alt={file.name} />
+										<Img src={URL.createObjectURL(file)} alt={file.name} />
 										<button onClick={() => handleFileDelete(file)}>Delete</button>
 									</li>
 								))}
@@ -113,7 +113,7 @@ function ReviewInput() {
 						)}
 					</div>
 				</div>
-				<Button type="submit" onClick={reviewRegister}>
+				<Button type="submit" onClick={reviewRegister} id="register-review">
 					리뷰 등록하기
 				</Button>
 			</form>
--- frontend/src/styles/Review/ReviewInputstyle.ts
+++ frontend/src/styles/Review/ReviewInputstyle.ts
@@ -5,7 +5,8 @@ export const Form = styled.input.attrs({
 })`
 	width: 100%;
 	height: 124px;
-	box-shadow: 4px 1px 1px #d9d9d9;
+	border-color: #d9d9d9;
+	box-shadow: 2px 4px 1px 1px #d9d9d9;
 	margin-top: 20px;
 	margin-bottom: 44px;
 `;
@@ -19,7 +20,24 @@ export const StarContainer = styled.div`
 	display: flex;
 	text-align: center;
 	margin: 13px 0px;
+	margin-bottom: 40px;
 	.active {
 		color: #ffd600;
 	}
+`;
+
+export const Img = styled.img`
+	width: 100px;
+	height: 100px;
+`;
+
+export const Input = styled.input`
+	position: absolute;
+	width: 0;
+	height: 0;
+	padding: 0;
+	margin: -1px;
+	overflow: hidden;
+	clip: rect(0, 0, 0, 0);
+	border: 0;
 `;
