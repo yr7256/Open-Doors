8f5e85ccc109376fab6e20925c8a08dfc260f442
yeahzizi <newyeahzizi@gmail.com>
2023-03-30 22:53:53 +0900

    chore: 이름 추가

--- frontend/src/Components/Recommend/ShowRecommend.tsx
+++ frontend/src/Components/Recommend/ShowRecommend.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+type UserState = {
+	user: {
+		username: string;
+	};
+};
 
 function showRecommend() {
+	const userName = useSelector((state: UserState) => state.user.username);
 	return (
 		<>
-			<p>를 위한 오늘의 추천</p>
+			<p>{userName}을 위한 오늘의 추천</p>
 		</>
 	);
 }
