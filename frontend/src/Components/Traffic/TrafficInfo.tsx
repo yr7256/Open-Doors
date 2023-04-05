5e5926bfc4d69de2b76c36df7bd0b279f5bfb131
CessnaJ <eon8748@gmail.com>
2023-04-05 15:52:06 +0900

    modify: Traffic info component, page modified. connection test needed

--- frontend/src/Components/Traffic/TrafficInfo.tsx
+++ frontend/src/Components/Traffic/TrafficInfo.tsx
@@ -1,11 +1,70 @@
 import React from 'react';
+import bus from '../../assets/img/bus.png';
+import subway from '../../assets/img/subway.png';
+import calltaxi from '../../assets/img/calltaxi.png';
+
+import {
+	TrafficInfoHeader,
+	TrafficInfoHeaderContainer,
+	LineDiv,
+	TaxiCallDiv,
+	TrafficTitleWrapper,
+	TrafficInfoBox,
+	BusArrDiv,
+} from '../../styles/Traffic/TrafficStyled';
 
 const TrafficInfo = () => {
+	function LinkHandler() {
+		window.open('https://www.djcall.or.kr/', '_blank');
+	}
+
 	return (
 		<>
-			<p>여기는 교통정보입니다.</p>
+			<TrafficInfoHeaderContainer>
+				<TrafficInfoHeader>
+					<span>주변 </span>
+					<em>300m </em>
+					<span>교통 정보</span>
+				</TrafficInfoHeader>
+			</TrafficInfoHeaderContainer>
+			<LineDiv />
+			<TaxiCallDiv onClick={LinkHandler}>
+				<img src={calltaxi} alt="수화기사진" />
+				<span>콜택시를 이용한다면?</span>
+				<a>장애인 콜택시 예약</a>
+			</TaxiCallDiv>
+			<LineDiv />
+			<TrafficTitleWrapper>
+				<span>주변 </span>
+				<em>저상버스</em>
+				<span>/지하철</span>
+			</TrafficTitleWrapper>
+
+			{/* 아래는 map 돌려서.. 삼항연산자로 없으면 없다고.. */}
+			<TrafficInfoBox>
+				<img src={bus} alt="버스아이콘"></img>
+				<div className="busStopInfo">
+					<span className="busStopTitle">{'삼성화재연수원'}</span>
+					<span className="busStopNum">{'(11052)'}</span>
+				</div>
+				<div className="horizontalLine" />
+
+				{/* BusArrDiv 비어있으면? */}
+				<BusArrDiv>
+					<div className="flexrow">
+						<div className="">
+							<em>{'102'}</em>
+						</div>
+						<div className="spanContainer">
+							<span className="min">{4}분</span>
+							<span className="position">{4}정류장</span>
+						</div>
+					</div>
+					<div className="direction">{'한밭대학교방면'}</div>
+				</BusArrDiv>
+			</TrafficInfoBox>
 		</>
 	);
-}
+};
 
 export default TrafficInfo;
---
+++ frontend/src/styles/Traffic/TrafficStyled.ts
@@ -0,0 +1,149 @@
+import styled from 'styled-components';
+
+export const TrafficInfoHeaderContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+`;
+
+export const TrafficInfoHeader = styled.div`
+	align-self: center;
+	& > span {
+		font-size: 1.4rem;
+	}
+
+	& > em {
+		font-size: 1.4rem;
+		color: #4a6e98;
+	}
+`;
+
+export const LineDiv = styled.div`
+	width: 95%;
+	border-top: 2px solid #d9d9d9;
+	align-self: center;
+	margin: 2%;
+`;
+
+// 아래 Div 자체에 link를 걸어야됨.
+export const TaxiCallDiv = styled.div`
+	padding: 3% 0% 3% 0%;
+	display: flex;
+	align-items: center;
+
+	& > img {
+		width: 10%;
+		height: 10%;
+		margin-right: 1%;
+	}
+
+	& > span {
+		// font-size: ;
+	}
+
+	& > a {
+		// font-size: ;
+		color: #6393cb;
+	}
+`;
+
+export const TrafficTitleWrapper = styled.div`
+	padding: 3% 3% 3% 5%;
+	& > span {
+		font-size: 1.3rem;
+	}
+
+	& > em {
+		font-size: 1.3rem;
+		font-weight: bold;
+	}
+`;
+
+export const TrafficInfoBoxWrapper = styled.div``;
+
+// export const LineFullWidth = styled.div`
+//     width: 95%
+//     border-top: 1px solid #d9d9d9;
+
+//     & .
+
+// `;
+
+export const TrafficInfoBox = styled.div`
+	border-radius: 5px;
+	border: 2px solid #d9d9d9;
+
+	& > .busStopTitle {
+		font-size: 1rem;
+		margin-bottom: 1.5%;
+	}
+
+	& > .busStopNum {
+		font-size: 0.8rem;
+		margin-bottom: 1.5%;
+	}
+
+	& > .busStopInfo {
+		display: inline-block;
+		margin-bottom: 2%;
+	}
+
+	& > img {
+		display: inline-block;
+		margin-top: 1.5%;
+		margin-left: 1.5%;
+		// margin: 1.5%;
+		// padding: 1.5%;
+		// margin-left: 1%;
+		// margin-right: 1%;
+		width: 8%;
+		height: 8%;
+	}
+
+	& > .horizontalLine {
+		width: 100%;
+		border-top: 2px solid #d9d9d9;
+	}
+`;
+
+export const BusArrDiv = styled.div`
+	width: 100%;
+	padding-left: 5%;
+	padding-right: 5%;
+	padding-top: 2%;
+	padding-bottom: 2%;
+
+	& .flexrow {
+		display: flex;
+		justify-content: space-between;
+
+		& > div > em {
+			font-weight: bold;
+			font-size: 1.15rem;
+		}
+
+		& .spanContainer {
+			display: flex;
+			justify-content: space-between;
+			width: 30%;
+		}
+
+		& .min {
+			// margin-right: 2%; // 여기에 원하는 간격을 추가하세요. (2% 혹은 16px)
+			font-size: 1rem;
+			color: #e91818;
+		}
+
+		& .position {
+			// margin-left: 1%;
+
+			font-size: 1rem;
+		}
+	}
+
+	& .direction {
+		text-align: right;
+		font-size: 0.8rem;
+		color: #6393cb;
+	}
+`;
