5dde634639e91ae237546b6beea290470d0c496b
yr7256 <yr7256@gmail.com>
2023-04-05 13:00:50 +0900

    feat: 내가 등록한 장소

---
+++ frontend/src/styles/MyLocation/Mylocation.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MylocContainer = styled.div`
+  width: 86vw;
+  height: 30vh;
+  background-color: #EFF4FA;
+  margin: 24px auto;
+  padding: 2vh 2vh 1vh 2vh;
+  border-radius: 8px;
+  & > * {
+    margin: 1vh 0 1vh 0;
+  }
+`
